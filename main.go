a1c2cbaf69a3825c2e6090c97a712c7536f66c7e
Riki Permana <dwi@neuronworks.co.id>
2022-09-30 00:32:44 +0700

    change run mode

--- main.go
+++ main.go
@@ -1,28 +1,24 @@
 package main
 
 import (
-	"log"
+	"os"
 
 	"github.com/ArwahDevops/gin-postgresql-api/config"
 	"github.com/ArwahDevops/gin-postgresql-api/routes"
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
 )
 
-func init() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Failed to load env file")
-	}
-}
-
 func main() {
-	router := gin.New()
+	// Running Mode
+	config.RunMode()
+	// Database
 	config.ConnectDB()
+	router := gin.Default()
 
 	routes.PingRoutes(router)
 	routes.UserRoutes(router)
 
-	router.Run(":8080")
+	appPort := os.Getenv("APP_PORT")
+	router.Run(":" + appPort)
 
 }
