785d0d7f8e7ee66e684fc967ce1db65c8c2c711a
Riki Permana <rikipermana@live.com>
2023-01-29 21:39:49 +0700

    uncomment

--- controller/user.controller.go
+++ controller/user.controller.go
@@ -23,19 +23,15 @@ func checkPassword(hashedPassword, password string) bool {
 func RegisterUser(c *gin.Context) {
 	var user models.User
 	c.BindJSON(&user)
-
-	// Cek apakah email sudah terdaftar
 	var existingUser models.User
 	config.DB.Where("email = ?", user.Email).First(&existingUser)
 	if existingUser.ID != 0 {
-		// Email sudah terdaftar, kirim pesan error ke pengguna
 		c.JSON(http.StatusBadRequest, gin.H{
 			"error": "Email sudah terdaftar",
 		})
 		return
 	}
 
-	// Mengenkripsi password pengguna sebelum disimpan ke dalam database
 	hashedPassword, err := hashPassword(user.Password)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
@@ -50,47 +46,37 @@ func RegisterUser(c *gin.Context) {
 }
 
 func UserLogin(c *gin.Context) {
-	// Ambil email dan password dari request
 	var request struct {
 		Email    string `json:"email" binding:"required"`
 		Password string `json:"password" binding:"required"`
 	}
 	if err := c.BindJSON(&request); err != nil {
-		// Kirim response error jika email atau password kosong
 		c.JSON(http.StatusBadRequest, gin.H{
 			"error": "Email atau password tidak boleh kosong",
 		})
 		return
 	}
-	// Cari pengguna dengan email yang sesuai
 	var user models.User
 	if err := config.DB.Where("email = ?", request.Email).First(&user).Error; err != nil {
-		// Kirim response error jika email atau password salah
 		c.JSON(http.StatusUnauthorized, gin.H{
 			"error": "Email atau password salah",
 		})
 		return
 	}
-	// Verifikasi password pengguna
 	if !checkPassword(user.Password, request.Password) {
-		// Kirim response error jika email atau password salah
 		c.JSON(http.StatusUnauthorized, gin.H{
 			"error": "Email atau password salah",
 		})
 		return
 	}
-	// Generate JWT untuk pengguna
 	token, err := middleware.GenerateJWT(uint64(user.ID), user.Email)
 	if err != nil {
-		// Kirim response error jika gagal membuat JWT
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"error": "Gagal membuat JWT",
 		})
 		return
 	}
-	// Kirim JWT ke client melalui header Authorization
 	c.Header("Authorization", "Bearer "+token)
-	// Kirim response sukses ke client
 	c.JSON(200, gin.H{
 		"message": "Login sukses",
 		"jwt":     token,
@@ -119,7 +105,6 @@ func UpdateUser(c *gin.Context) {
 	}
 	c.BindJSON(&user)
 
-	// Mengenkripsi password pengguna sebelum disimpan ke dalam database
 	hashedPassword, err := hashPassword(user.Password)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
--- go.mod
+++ go.mod
@@ -3,9 +3,16 @@ module github.com/ArwahDevops/gin-postgresql-api
 go 1.19
 
 require (
-	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gin-gonic/gin v1.8.1
+	github.com/joho/godotenv v1.4.0
+	golang.org/x/crypto v0.0.0-20220817201139-bc19a97f63c8
+	gorm.io/driver/postgres v1.3.9
+	gorm.io/gorm v1.23.8
+)
+
+require (
 	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/gin-gonic/gin v1.8.1 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.11.0 // indirect
@@ -20,7 +27,6 @@ require (
 	github.com/jackc/pgx/v4 v4.17.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	github.com/joho/godotenv v1.4.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
@@ -28,12 +34,9 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.3 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
-	golang.org/x/crypto v0.0.0-20220817201139-bc19a97f63c8 // indirect
 	golang.org/x/net v0.0.0-20220812174116-3211cb980234 // indirect
 	golang.org/x/sys v0.0.0-20220818161305-2296e01440c6 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
-	gorm.io/driver/postgres v1.3.9 // indirect
-	gorm.io/gorm v1.23.8 // indirect
 )
